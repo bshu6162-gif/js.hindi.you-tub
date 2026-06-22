97cdd792f4f34d4572151fcdda0e753048f5bb67
bshu6162-gif <bshu6162@gmail.com>
2026-06-22 11:29:26 +0000

    variables and constant

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountid=11223
+let accountemal="bshu6162@gmail.com"
+console.table([]);
+/*
+we caanot used var(variables) because of issues in block scope and functional scope
+*/
--- README.md
+++
@@ -1,2 +0,0 @@
-# js.hindi.you-tub
-a code rapa for javascript series at chai or code  you tube channel
