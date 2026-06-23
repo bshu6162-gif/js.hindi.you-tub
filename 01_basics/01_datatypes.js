4f219a62b125c1aae34d5ac86a58da9c59c13d80
bshu6162-gif <bshu6162@gmail.com>
2026-06-23 04:40:18 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,17 @@
+let name="ekta"
+let age=18
+let isloggedIn=false
+console.log(typeof "ekta" );
+
+
+
+/*
+#data type (premitive)
+*/
+//numer=>2 to power 53 approx
+//bigint=>used to large value
+//string=>"" or ' '
+//null=standalone value type is always object
+//undefined=>value is not assign but variable also declare type is always undefined
+// symbol=>unique
+
