4d2645cbab8d6661073c53cf243deeffad98340b
bshu6162-gif <bshu6162@gmail.com>
2026-06-23 06:04:03 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -3,3 +3,6 @@ console.log(typeof score);
 console.log(typeof(score));
 
 let valueInNumber=Number(score)//it is the way to conversion from one form to another form
+    console.log(33);
+    let idtrue=true(valueInNumber);
+    
